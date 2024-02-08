0ee5608bd063e91819aba119d86d3a7e1f3c8a92
normaliner <prokopenko.viktor110701@mail.ru>
2024-02-08 20:44:42 +0300

    HomeWork_03

--- app.js
+++ app.js
@@ -44,4 +44,103 @@ const arrString = ['Hello', ' wolrd ', '!'];
 const joinStringArray = arrString.reduce((acc, value) => acc.concat(value));
 
 // 3.2
+class Stack {
+  constructor() {
+    this.arr = [];
+  }
+  // Определение на пустоту
+  isEmpty() {
+    return !Boolean(this.arr.length);
+  }
+  // Добавление элемента в стэк
+  push(value) {
+    this.arr.push(value);
+  }
+  // Удаление и получение последнего элемента стэка
+  pop() {
+    if (this.isEmpty()) {
+      return 'Out of range';
+    }
+    return this.arr.pop();
+  }
+  // Получение последнего элемента без удаления
+  peek() {
+    if (this.isEmpty()) {
+      return 'Out of range';
+    }
+    return this.arr[this.arr.length - 1];
+  }
+  // Получение длины стэка
+  size() {
+    return this.arr.length;
+  }
+  // Очищение стэка
+  clear() {
+    this.arr = [];
+  }
+  // Вывод стэка
+  print() {
+    console.log(this.arr.toString());
+  }
+}
+const stack = new Stack();
 
+// 3.3
+class Queue {
+  constructor() {
+    this.arr = [];
+  }
+  // Определение на пустоту
+  isEmpty() {
+    return this.arr.length === 0;
+  }
+  // Добавление элемента в очередь
+  push(value) {
+    this.arr.push(value);
+  }
+  // Удаление первого элемента очереди
+  pop() {
+    return this.arr.shift();
+  }
+  // Получение первого элемента без удаления
+  front() {
+    return this.arr[0];
+  }
+  // Получение последнего элемента без удаления
+  back() {
+    return this.arr[this.arr.length - 1];
+  }
+  // Получение длины очереди
+  size() {
+    return this.arr.length;
+  }
+  // Вывод очереди
+  print() {
+    console.log(this.arr.toString());
+  }
+}
+const queue = new Queue();
+
+// Bonus task
+Function.prototype.bind = function (context) {
+  let fn = this; // Исходная функция
+  let args = Array.prototype.slice.call(arguments, 1); // Аргументы, переданные в bind, начиная со второго
+
+  // Возвращаем функцию, которая вызывает исходную функцию fn с заданным контекстом и аргументами
+  return function () {
+    let bindArgs = Array.prototype.slice.call(arguments); // Аргументы, переданные при вызове
+    return fn.apply(context, args.concat(bindArgs)); // Объединяем аргументы из bind и вызова
+  };
+};
+
+function sayHi() {
+  console.log(`Hello ${this.name}`);
+}
+
+const obj1 = { name: 'Viktor' };
+const obj2 = { name: 'Maria' };
+
+const test1 = sayHi.bind(obj1)
+test1();
+const test2 = sayHi.bind(obj2)
+test2();
